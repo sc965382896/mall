3e7112ccc7fbc37ced0dd3949011963edc4908aa
sc965382896 <35653894+sc965382896@users.noreply.github.com>
2020-04-24 15:41:12 +0800

    learn mybatis

--- src/test/java/com/imooc/mall/MallApplicationTests.java
+++ src/test/java/com/imooc/mall/MallApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class MallApplicationTests {
+public class MallApplicationTests {
 
     @Test
     void contextLoads() { }
